e174da3ad725963abd75fe347cd6672644c52f41
Roman <rm07ru@gmail.com>
2017-12-16 21:59:07 +0300

    Add results store

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -34,6 +34,19 @@ class Counter extends Component {
                 <CounterControl label="Decrement" clicked={this.props.onDecrementCounter} />
                 <CounterControl label="Add 5" clicked={this.props.onAddCounter} />
                 <CounterControl label="Subtract 5" clicked={this.props.onSubstractCounter} />
+                <hr />
+                <button onClick={this.props.onStoreResult}>
+                    Store result
+                </button>
+                <ul>
+                    {
+                        this.props.storeResults.map(result => (
+                            <li key={result.id}>
+                                {result.value}
+                            </li>
+                        ))
+                    }
+                </ul>
             </div>
         );
     }
@@ -42,15 +55,18 @@ class Counter extends Component {
 const mapStateToProps = (state) => {
     return {
         ctr: state.counter,
+        storeResults: state.results,
     };
 };
 
 const mapDispatchToProps = (dispatch) => {
     return {
-        onIncrementCounter: () => dispatch({type: 'INCREMENT'}),
-        onDecrementCounter: () => dispatch({type: 'DECREMENT'}),
-        onAddCounter: () => dispatch({type: 'ADD', value: 5}),
-        onSubstractCounter: () => dispatch({ type: 'SUBSTRACT', value: 5}),
+        onIncrementCounter: () => dispatch({ type: 'INCREMENT' }),
+        onDecrementCounter: () => dispatch({ type: 'DECREMENT' }),
+        onAddCounter: () => dispatch({ type: 'ADD', value: 5 }),
+        onSubstractCounter: () => dispatch({ type: 'SUBSTRACT', value: 5 }),
+        onStoreResult: () => dispatch({ type: 'STORE_RESULT' }),
+        onRemoveResult: () => dispatch({ type: 'REMOVE_RESULT' })
     };
 };
 
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,25 +1,38 @@
 const initialState = {
-  counter: 12
+  counter: 12,
+  results: []
 };
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case 'INCREMENT':
       return {
+        ...state,
         counter: state.counter + 1
       };
     case 'DECREMENT':
       return {
+        ...state,        
         counter: state.counter -1
       };
     case 'ADD':
       return {
+        ...state,        
         counter: state.counter + action.value
       };
     case 'SUBSTRACT':
       return {
+        ...state,        
         counter: state.counter - action.value
       };
+    case 'STORE_RESULT':
+      return {
+        ...state,
+        results: state.results.concat({
+          value: state.counter,
+          id: new Date(),
+        })        
+      }
     default: 
       return state;
   }
